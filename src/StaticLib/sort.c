6377b0c9685d7f928ff7bffbfc8d141d066f987b
imagire <imagire@gmail.com>
2020-11-30 01:15:55 +0900

    バブルソートを実装

--- src/StaticLib/sort.c
+++ src/StaticLib/sort.c
@@ -6,8 +6,16 @@
 // ソート
 void bubble_sort(array* a)
 {
-	// ToDo: 実装してください
-	a->nodes = NULL;
+	for (int n = 0; n < a->num - 1; n++) {
+		for (int i = a->num - 1; n < i; i--)
+		{
+			if (a->nodes[i].key < a->nodes[i - 1].key) {
+				node tmp = a->nodes[i];
+				a->nodes[i] = a->nodes[i - 1];
+				a->nodes[i - 1] = tmp;
+			}
+		}
+	}
 }
 
 
